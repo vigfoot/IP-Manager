2b056a9a4278e24b4a5327bffb662fbd99aa5b4d
vigfoot <dev.vigfoot@gmail.com>
2023-03-07 16:29:46 +0900

    initialize - (finished basement) get local ip address
    Signed-off-by: Xasquatch <bigfoot7774@gmail.com>

--- src/main/java/com/vigfoot/ipmanager/controller/Controller.java
+++ src/main/java/com/vigfoot/ipmanager/controller/Controller.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 @RestController
 public class Controller {
 
     @GetMapping("/ip/verify")
     public Mono<Boolean> verifyConnection() {
+        try(Socket socket = new Socket()){
+            socket.connect(new InetSocketAddress("google.com", 80));
+            System.out.println(socket.getLocalAddress().getHostAddress());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return Mono.empty();
     }
 }
